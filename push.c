d47b5fc2a319732faf0f76db5a23f70c4f80cca5
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-10 18:20:19 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,18 +1,15 @@
 #include "monty.h"
 
-extern char *arg;
-
 /**
- * is_integer - checks if string is integer
- * @str: string to check
- *
- * Return: 1 if integer, 0 otherwise
+ * is_integer - checks valid integer
+ * @str: string
+ * Return: 1 or 0
  */
 int is_integer(char *str)
 {
 	int i = 0;
 
-	if (str == NULL)
+	if (!str)
 		return (0);
 
 	if (str[0] == '-')
@@ -28,35 +25,33 @@ int is_integer(char *str)
 }
 
 /**
- * push - pushes element onto stack
- * @stack: pointer to stack
- * @line_number: current line number
+ * push - pushes node to stack
+ * @stack: stack
+ * @line_number: line number
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node;
+	stack_t *new;
 
 	if (!is_integer(arg))
 	{
-		fprintf(stderr,
-			"L%d: usage: push integer\n",
-			line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(stack_t));
+	if (!new)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = atoi(arg);
-	new_node->prev = NULL;
-	new_node->next = *stack;
+	new->n = atoi(arg);
+	new->prev = NULL;
+	new->next = *stack;
 
 	if (*stack)
-		(*stack)->prev = new_node;
+		(*stack)->prev = new;
 
-	*stack = new_node;
+	*stack = new;
 }
