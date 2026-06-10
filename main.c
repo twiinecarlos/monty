05a4a47344d0b27c78189d3f6bd3e792001ab6d5
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-10 18:43:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
-file = fopen(argv[1], "r");
-if (file == NULL)
+#include "monty.h"
+
+char *arg = NULL;
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: arguments
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
 {
-	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-	exit(EXIT_FAILURE);
+	FILE *file;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	read_file(file, &stack);
+
+	fclose(file);
+	free_stack(stack);
+
+	return (0);
 }
