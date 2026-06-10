fa0109bcdef938df884acf7a71a3635003a8663f
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-10 17:48:16 +0200

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,6 +1,84 @@
 #include "monty.h"
 
-char *push_arg = NULL;
+bus_t bus = {NULL, NULL, NULL, NULL};
+
+/**
+ * free_stack - frees all nodes in the stack
+ * @stack: pointer to the top node
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *tmp;
+
+	while (stack)
+	{
+		tmp = stack->next;
+		free(stack);
+		stack = tmp;
+	}
+}
+
+/**
+ * run_line - dispatches a single opcode line
+ * @stack: pointer to stack pointer
+ * @line_number: current line number
+ */
+void run_line(stack_t **stack, unsigned int line_number)
+{
+	char *opcode;
+	int i, found;
+	instruction_t instructions[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{NULL, NULL}
+	};
+
+	opcode = strtok(bus.line, " \t");
+	if (!opcode || opcode[0] == '#')
+		return;
+
+	bus.push_arg = strtok(NULL, " \t");
+	found = 0;
+	for (i = 0; instructions[i].opcode; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			found = 1;
+			break;
+		}
+	}
+	if (!found)
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+		free_stack(*stack);
+		free(bus.line);
+		fclose(bus.fp);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * run_file - reads and executes every line in the bytecode file
+ */
+void run_file(void)
+{
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+
+	while ((read = getline(&bus.line, &len, bus.fp)) != -1)
+	{
+		line_number++;
+		if (bus.line[read - 1] == '\n')
+			bus.line[read - 1] = '\0';
+		run_line(&stack, line_number);
+	}
+	free_stack(stack);
+	free(bus.line);
+	fclose(bus.fp);
+}
 
 /**
  * main - entry point for the monty interpreter
@@ -11,84 +89,18 @@ char *push_arg = NULL;
  */
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
-	char *opcode;
-	instruction_t instructions[] = {
-		{"push", op_push},
-		{"pall", op_pall},
-		{NULL, NULL}
-	};
-	int i, found;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	fp = fopen(argv[1], "r");
-	if (!fp)
+	bus.filename = argv[1];
+	bus.fp = fopen(bus.filename, "r");
+	if (!bus.fp)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", bus.filename);
 		exit(EXIT_FAILURE);
 	}
-
-	while ((read = getline(&line, &len, fp)) != -1)
-	{
-		line_number++;
-
-		/* strip newline */
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
-
-		opcode = strtok(line, " \t");
-
-		/* skip empty lines and comments */
-		if (!opcode || opcode[0] == '#')
-			continue;
-
-		/* save argument for push */
-		push_arg = strtok(NULL, " \t");
-
-		found = 0;
-		for (i = 0; instructions[i].opcode; i++)
-		{
-			if (strcmp(opcode, instructions[i].opcode) == 0)
-			{
-				instructions[i].f(&stack, line_number);
-				found = 1;
-				break;
-			}
-		}
-
-		if (!found)
-		{
-			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-			free(line);
-			fclose(fp);
-			/* free stack */
-			while (stack)
-			{
-				stack_t *tmp = stack->next;
-				free(stack);
-				stack = tmp;
-			}
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	free(line);
-	fclose(fp);
-	while (stack)
-	{
-		stack_t *tmp = stack->next;
-		free(stack);
-		stack = tmp;
-	}
+	run_file();
 	return (EXIT_SUCCESS);
 }
