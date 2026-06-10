bd7283d81d018e1256b99bc9e3d8d19474b1e46e
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-10 18:20:53 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,19 +1,19 @@
 #include "monty.h"
 
 /**
- * pall - prints all stack values
- * @stack: pointer to stack
- * @line_number: current line number
+ * pall - prints stack
+ * @stack: stack pointer
+ * @line_number: line number
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
+	stack_t *temp = *stack;
 
 	(void)line_number;
 
-	while (current)
+	while (temp)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
