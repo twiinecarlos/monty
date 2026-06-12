ddcf51a4a35a35e02ef7b77e190fd793957d04b9
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 17:56:10 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,18 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define _GNU_SOURCE
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * struct stack_s - doubly linked list node
- * @n: integer
- * @prev: previous node
- * @next: next node
- */
 typedef struct stack_s
 {
 	int n;
@@ -20,14 +12,10 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-/* global argument for push */
-extern char *value;
-
-/* functions */
 void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
 
-void execute(char *opcode, stack_t **stack, unsigned int line);
+void execute(char *opcode, char *arg, stack_t **stack, unsigned int line);
 void read_file(FILE *file, stack_t **stack);
 
 int is_integer(char *str);
