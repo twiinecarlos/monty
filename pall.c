36ef2125aeebc3dad6368e3d04466c9580ee96a7
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 17:24:23 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -2,13 +2,13 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+	stack_t *tmp = *stack;
 
 	(void)line_number;
 
-	while (temp)
+	while (tmp)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
 	}
 }
