6fbf205d8fa2734d347ab9f20729dc7be777c0e1
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 16:22:31 +0200

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	(void)line_number;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
