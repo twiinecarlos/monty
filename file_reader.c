77ff2f506ab315797808219b229e43462210dd48
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 16:23:43 +0200

    Create file_reader.c

---
+++ file_reader.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+void read_file(FILE *file, stack_t **stack)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	char *opcode;
+	char *arg;
+
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+
+		opcode = strtok(line, " \t\n");
+		arg = strtok(NULL, " \t\n");
+
+		if (!opcode)
+			continue;
+
+		execute(opcode, arg, stack, line_number);
+	}
+
+	free(line);
+}
