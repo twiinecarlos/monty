91255c01b0d59bcfea5bfa612d25bf9818949003
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 16:29:40 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,5 +1,3 @@
-#include "monty.h"
-
 int is_integer(char *str)
 {
 	int i = 0;
@@ -7,8 +5,11 @@ int is_integer(char *str)
 	if (!str)
 		return (0);
 
-	if (str[0] == '-')
+	if (str[0] == '-' || str[0] == '+')
 		i++;
+
+	if (str[i] == '\0')
+		return (0);
 
 	for (; str[i]; i++)
 	{
@@ -16,31 +17,4 @@ int is_integer(char *str)
 			return (0);
 	}
 	return (1);
-}
-
-void push(stack_t **stack, unsigned int line_number, char *arg)
-{
-	stack_t *new_node;
-
-	if (!is_integer(arg))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	new_node = malloc(sizeof(stack_t));
-	if (!new_node)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = atoi(arg);
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack)
-		(*stack)->prev = new_node;
-
-	*stack = new_node;
 }
