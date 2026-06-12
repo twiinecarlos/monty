e3d5ebded49c8886a6a564c3556aaf8205be4bb0
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 16:22:54 +0200

    Create executor.c

---
+++ executor.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+void execute(char *opcode, char *arg, stack_t **stack, unsigned int line)
+{
+	if (strcmp(opcode, "push") == 0)
+		push(stack, line, arg);
+	else if (strcmp(opcode, "pall") == 0)
+		pall(stack, line);
+	else
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line, opcode);
+		exit(EXIT_FAILURE);
+	}
+}
