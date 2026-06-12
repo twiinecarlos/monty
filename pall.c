595b28ab24053e12681de3c9f11041348c2d7c74
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-06-12 17:57:57 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,5 +1,8 @@
-#include "monty.h"
-
+/**
+ * pall - prints all stack values
+ * @stack: pointer to stack
+ * @line_number: line number
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
