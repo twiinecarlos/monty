9a8cd3c03f4d2359838fed1d6b92c0285cddcf93
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-07-11 22:21:10 +0200

    Add swap command handling in executor

--- executor.c
+++ executor.c
@@ -17,6 +17,8 @@ void execute(char *opcode, char *arg, stack_t **stack, unsigned int line)
 		pint(stack, line);
 	else if (strcmp(opcode, "pop") == 0)
 		pop(stack, line);
+	else if (strcmp(opcode, "swap") == 0)
+		swap(stack, line);
 	else
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line, opcode);
