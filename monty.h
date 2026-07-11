dfb24bf16ce3647b57789900a4ada6c868663248
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-07-11 22:27:56 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -17,6 +17,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 
 void execute(char *opcode, char *arg, stack_t **stack, unsigned int line);
 void read_file(FILE *file, stack_t **stack);
