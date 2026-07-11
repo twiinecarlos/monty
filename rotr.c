66826d8fb94d432520c70491ad8fda7b4ec9117c
twiinecarlos <99586350+twiinecarlos@users.noreply.github.com>
2026-07-11 23:04:12 +0200

    Implement rotr function to rotate stack to bottom

---
+++ rotr.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates the stack to the bottom
+ * @stack: pointer to the stack
+ * @line_number: line number of the opcode
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *last;
+
+	(void)line_number;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	last = *stack;
+
+	while (last->next)
+		last = last->next;
+
+	last->prev->next = NULL;
+	last->prev = NULL;
+	last->next = *stack;
+	(*stack)->prev = last;
+
+	*stack = last;
+}
